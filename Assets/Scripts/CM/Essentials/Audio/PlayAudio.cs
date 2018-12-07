5e3f9ff306eced68156f4ebd718025f299d0e55d
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-12-07 18:08:26 +0100

    Added walking audio

---
+++ Assets/Scripts/CM/Essentials/Audio/PlayAudio.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CM.Essentials.Audio
+{
+	public class PlayAudio : MonoBehaviour
+	{
+		[SerializeField] private AudioSource _audioSource;
+
+		public void Play()
+		{
+			_audioSource.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/CM/Essentials/Audio/WalkAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CM.Orientation;
+
+namespace CM.Essentials.Audio
+{
+	public class WalkAudio : MonoBehaviour
+	{
+		[SerializeField] private AudioSource _audioSource;
+		[SerializeField] private RigidbodyMovement _rigidbodyMovement;
+
+		private void Update()
+		{
+			if (_audioSource.isPlaying && !_rigidbodyMovement.IsMoving)
+				_audioSource.Stop();
+		}
+	}
+}
