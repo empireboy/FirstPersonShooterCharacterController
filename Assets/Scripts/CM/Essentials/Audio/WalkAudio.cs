94ff0c6648c362b07399746dd4e835f5bd33e8b1
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-12-07 21:31:18 +0100

    Walk audio transition

--- Assets/Scripts/CM/Essentials/Audio/WalkAudio.cs
+++ Assets/Scripts/CM/Essentials/Audio/WalkAudio.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using CM.Orientation;
 
 namespace CM.Essentials.Audio
@@ -9,11 +7,26 @@ namespace CM.Essentials.Audio
 	{
 		[SerializeField] private AudioSource _audioSource;
 		[SerializeField] private RigidbodyMovement _rigidbodyMovement;
+		[SerializeField] private float _stopAudioSpeed = 1f;
+
+		private bool _isStopping = false;
 
 		private void Update()
 		{
 			if (_audioSource.isPlaying && !_rigidbodyMovement.IsMoving)
-				_audioSource.Stop();
+				_isStopping = true;
+
+			if (_isStopping)
+			{
+				_audioSource.volume -= Time.deltaTime * _stopAudioSpeed;
+
+				if (_audioSource.volume <= 0)
+				{
+					_audioSource.Stop();
+					_isStopping = false;
+					_audioSource.volume = 1;
+				}
+			}
 		}
 	}
 }
