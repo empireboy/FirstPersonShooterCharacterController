7f8c27162c1955599baf17ba9f678f705b1b6f3f
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-12-16 14:42:14 +0100

    Added a drunk shader

---
+++ Assets/Scripts/CM/Shader/PostEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Shader
+{
+	[ExecuteInEditMode]
+	public class PostEffect : MonoBehaviour
+	{
+		[SerializeField] private Material _material;
+
+		private void OnRenderImage(RenderTexture source, RenderTexture destination)
+		{
+			Graphics.Blit(source, destination, _material);
+		}
+	}
+}
